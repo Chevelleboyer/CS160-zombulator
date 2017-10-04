cf405836fee05c55f5bcddf5930d8875a2a28399
Chevelle Boyer <chevelleboyer@gmail.com>
2017-10-04 09:51:28 -0700

    Made circles move.

--- Sketch.js
+++ Sketch.js
@@ -1,13 +1,19 @@
 // Zombulator by Chevelle Boyer
 
+var zombieX = 50;
+var zombie2X = 100;
+
 function setup() {
 createCanvas(800,800);
 }
 
 function draw() {
+	background(255,255,255);
 	fill(255,0,0);
-ellipse(50,50,80,80);
+ellipse(zombieX,50,80,80);
 fill(150,150,200);
-ellipse(200,100,80,80);
+ellipse(zombie2X,100,80,80);
+zombieX = zombieX + 2;
+zombie2X = zombie2X +3;
 }
 
