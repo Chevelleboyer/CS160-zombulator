af17f99725094f8631a76d032a4720ec51a87761
Chevelle Boyer <chevelleboyer@gmail.com>
2017-10-09 07:58:04 -0700

    Added third zombie.

--- Sketch.js
+++ Sketch.js
@@ -3,6 +3,7 @@
 var zombieX = 50;
 var zombie2X = 100;
 var zombie3x = 50;
+var zombie4x = 300;
 
 function setup() {
 	createCanvas(800, 800);
@@ -23,9 +24,14 @@ function draw() {
 	strokeWeight(10);
 	stroke(0, 255, 4);
 	ellipse(zombie3x, 80, 50, 80);
+	fill(0, 12, 91);
+	strokeWeight(8);
+	stroke(2, 237, 29);
+	ellipse(300, zombie4x, 50, 50);
 zombieX = zombieX + 2;
 zombie2X = zombie2X + 3;
 zombie3x = zombie3x + 4;
+zombie4x = zombie4x + 1;
 
 if (zombieX >= 800) {
 	zombieX = 100;
