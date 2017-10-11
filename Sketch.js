027be4f6871cfd5a32a4142287d2ed5633382d77
Chevelle Boyer <chevelleboyer@gmail.com>
2017-10-10 19:41:27 -0700

    Made human bounce, made zombie color=random greens, human color=random tans.

--- Sketch.js
+++ Sketch.js
@@ -1,42 +1,48 @@
 // Zombulator by Chevelle Boyer
 
 var zombieY = 50;
-var humanY = 100;
 var zombieColor;
-var humanColor;
 var backgroundColor;
 var zombieV = 0;
 var zombieA = 0.2;
 var zombieSize = 80;
-var zombieDamping = -0.7;
-
+var zombieDamping = -0.5;
+var humanY = 100;
+var humanColor;
+var humanV = 0;
+var humanA = 0.7;
+var humanSize = 100;
+var humanDamping = -0.8;
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
-	zombieColor = color(0, 29, 255);
-	humanColor = color(150, 150, 200);
+	zombieColor = color(192, 255, 160);
+	humanColor = color(255, 245, 186);
 	backgroundColor = (186, 186, 186);
 }
 
 function draw() {
 	background(backgroundColor);
 	fill(zombieColor);
-	noStroke();
+	stroke(186, 0, 0);
+	strokeWeight(8);
 	ellipse(50, zombieY, zombieSize, zombieSize);
 	zombieY += zombieV;
 	zombieV += zombieA;
 if (zombieY + (zombieSize / 2) >= windowHeight) {
 	zombieY = windowHeight - (zombieSize / 2);
 	zombieV *= zombieDamping;
-	zombieColor = color(random(255), random(255), random(255));
+	zombieColor = color(random(147, 70), random(247,119), 27);
 }
 	fill(humanColor);
 	noStroke();
-	ellipse(500, humanY, 80, 80);
-	humanY += 4;
-if (humanY >= windowWidth) {
-	humanY = 0;
-	humanColor = color(random(255), random(255), random(255));
+	ellipse(500, humanY, humanSize, humanSize);
+	humanY += humanV;
+	humanV += humanA;
+if (humanY + (humanSize / 2) >= windowHeight) {
+	humanY = windowHeight - (zombieSize / 2);
+	humanV *= humanDamping;
+	humanColor = color(255, random(245, 225), random(186, 0));
 }
 
 }
